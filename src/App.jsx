9135f6192274aa3e123659cee718fb0090ccf2b0
jeylanab <jeylantesi@gmail.com>
2024-03-25 05:50:50 +0000

    added hero section file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from "./style";
 import NavBar from "./component/NavBar";
+import Hero from "./component/Hero";
 
 const App = () => (
   <div >
     <div className="lg:m-10 m-3 ">
-      <NavBar/>
+      <NavBar />
+      <Hero/>
       
     </div>
   </div>
