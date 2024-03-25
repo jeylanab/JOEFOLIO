b4864b430913edbe0de2da2d257657daa33c78b1
jeylanab <jeylantesi@gmail.com>
2024-03-25 18:25:50 +0000

    added card

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -6,7 +6,7 @@ const Project = () => {
       <div>
             <div className='whatDo font-semibold lg:text-4xl text-3xl text-center py-10'>My Projects</div>
             <div className='projectcard flex justify-center items-center'>
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+                <div className='grid grid-cols-1 lg:grid-cols-3 shadow-md gap-4'>
                     {Projects.map((project, index) => (
                     <div className='m-10 border text-center p-3 relative' key={index}>
                         <div className='whatDo text-3xl'>{project.title}</div>
@@ -17,7 +17,7 @@ const Project = () => {
                         </div>
                         {/* Image element for project picture */}
                         <img
-                        className='absolute top-0 left-0 w-full h-full object-cover opacity-0 hover:opacity-100 transition-opacity duration-300'
+                        className='absolute top-0 left-0 w-full h-full  object-cover opacity-0 hover:opacity-70 transition-opacity duration-300'
                         src={project.imageUrl}
                         alt={project.title}
                         />
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,7 @@
-
+import haveWord from '../assets/haveWord.png'
+import quiz from '../assets/quiz.png'
+import toDo from '../assets/toDo.png'
+import weather from '../assets/weather.png'
 export const navLinks = [
   {
     id: "Home",
@@ -29,28 +32,33 @@ export const Projects = [
   {
     title: "Job Nest",
     description: "Job Nest is a job search platform that helps users find employment opportunities based on their skills, experience, and location. It provides features such as job listings, resume uploading, and application tracking.",
-    Technologies: "HTML, CSS, JavaScript (including Vanilla JS and jQuery), React.js, Bootstrap, Firebase (for authentication and data storage)"
+    Technologies: "HTML, CSS, JavaScript (including Vanilla JS and jQuery), React.js, Bootstrap, Firebase (for authentication and data storage)",
+    imageUrl: haveWord
   },
   {
     title: "Have A Word",
     description: "Have A Word is a dictionary and translation web application with quiz functionality. It allows users to search for words, view definitions, translations, and example sentences. Additionally, users can test their knowledge with quizzes on vocabulary and language proficiency.",
-    Technologies: "HTML, CSS, JavaScript, React.js, Tailwind CSS, and other relevant technologies."
+    Technologies: "HTML, CSS, JavaScript, React.js, Tailwind CSS, and other relevant technologies.",
+    imageUrl: haveWord
   }
   ,
   {
     title: "Quiz App",
     description: "Quiz App is an educational tool that allows users to create, take, and share quizzes on various topics. It includes features such as quiz creation, multiple-choice questions, scoring, and leaderboard.",
-    Technologies: "HTML, CSS, JavaScript (including Vanilla JS and jQuery), React.js, Bootstrap, Firebase (for authentication and data storage)"
+    Technologies: "HTML, CSS, JavaScript (including Vanilla JS and jQuery), React.js, Bootstrap, Firebase (for authentication and data storage)",
+    imageUrl: quiz
   },
   {
     title: "Weather DashBoard",
     description: "Weather Dashboard is a weather forecasting application that provides users with real-time weather information for their location and other cities worldwide. It offers features such as current conditions, hourly and daily forecasts, and weather alerts.",
-    Technologies: "HTML, CSS, JavaScript (including Vanilla JS and jQuery), React.js, Bootstrap, OpenWeatherMap API"
+    Technologies: "HTML, CSS, JavaScript (including Vanilla JS and jQuery), React.js, Bootstrap, OpenWeatherMap API",
+    imageUrl: weather
   },
   {
     title: "To Do List",
     description: "To-Do List is a task management application that helps users organize their daily tasks and activities. It allows users to create, edit, and delete tasks, set priorities, and mark tasks as completed.",
-    Technologies: "HTML, CSS, JavaScript (including Vanilla JS and jQuery), React.js, Bootstrap"
+    Technologies: "HTML, CSS, JavaScript (including Vanilla JS and jQuery), React.js, Bootstrap",
+    imageUrl: toDo
   }
 ]
 
