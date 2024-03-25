30b74ee7ad5f7adf0b732eb0e33c482be6cf5fc5
jeylanab <jeylantesi@gmail.com>
2024-03-25 19:05:56 +0000

    added

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -6,9 +6,9 @@ const Project = () => {
       <div>
             <div className='whatDo font-semibold lg:text-4xl text-3xl text-center py-10'>My Projects</div>
             <div className='projectcard flex justify-center items-center'>
-                <div className='grid grid-cols-1 lg:grid-cols-3 shadow-md gap-4'>
+                <div className='grid grid-cols-1 lg:grid-cols-3  gap-4'>
                     {Projects.map((project, index) => (
-                    <div className='m-10 border text-center p-3 relative' key={index}>
+                    <div className='m-10 border bg-emerald-500 rounded-md text-center p-3 relative' key={index}>
                         <div className='whatDo text-3xl'>{project.title}</div>
                         <div>{project.description}</div>
                         <div>
@@ -17,10 +17,14 @@ const Project = () => {
                         </div>
                         {/* Image element for project picture */}
                         <img
-                        className='absolute top-0 left-0 w-full h-full  object-cover opacity-0 hover:opacity-70 transition-opacity duration-300'
+                        className='absolute top-0 left-0 w-full h-full rounded-md object-cover opacity-0 hover:opacity-100 transition-opacity duration-300'
                         src={project.imageUrl}
                         alt={project.title}
-                        />
+                            />
+                            <div className='linkes flex z-20 justify-between items-end items-baseline m-10 content-end'>
+                                <div className='border p-2 rounded-md'>Deployed</div>      
+                                <div className='border p-2 rounded-md'>Repository</div>      
+                             </div>
                     </div> 
                     ))}
                 </div>
