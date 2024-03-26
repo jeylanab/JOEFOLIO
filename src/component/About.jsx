3b24521e31b00f5be3b0306d0718a48172f0757d
jeylanab <jeylantesi@gmail.com>
2024-03-26 18:47:39 +0000

    emailjs

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -27,12 +27,12 @@ const About = () => {
           <h1 className='whatDo font-semibold lg:text-4xl text-3xl text-center py-10'> Contact Me</h1>
          <form className='flex flex-col justify-center items-center ' ref={form} onSubmit={sendEmail}>
             <label className='whatDo '>Name</label>
-            <input className='lg:w-[400px]' type="text" name="user_name" />
+            <input className='lg:w-[400px] lg:h-[40px] w-[200px]' type="text" name="user_name" />
             <label className='whatDo'>Email</label>
-            <input className='lg:w-[400px]'  type="email" name="user_email" />
+            <input className='lg:w-[400px] lg:h-[40px] w-[200px]'  type="email" name="user_email" />
             <label className='whatDo'>Message</label>
-            <textarea className='lg:w-[400px]'  name="message" />
-            <input className='lg:w-[400px]'   type="submit" value="Send" />
+            <textarea className='lg:w-[400px] lg:h-[80px] w-[200px]'  name="message" />
+            <input className='lg:w-[400px] bg-purple-600 lg:h-[40px] my-5 rounded-lg w-[200px] p-3 text-slate-200'    type="submit" value="Send" />
          </form>
     </div>
   )
