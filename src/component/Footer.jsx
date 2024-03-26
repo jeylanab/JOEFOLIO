4b0e948c9ac93e015d9b0318fd0da8aa5a392115
jeylanab <jeylantesi@gmail.com>
2024-03-26 19:48:12 +0000

    added link

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
   return (
       <div className=' bg-black footer'>
           <div className='flex justify-center items-center'>
-              <div><FaGithub className='network' /></div>
-              <div><IoLogoLinkedin  className='network' /></div>
-              <div><FaDiscord className='network'  /></div>
+              <div><a href="https://github.com/jeylanab" target='blank'><FaGithub className='network' /></a></div>
+              <div><a href="https://www.linkedin.com/in/jeylan-a-tesi-53a746257/" target='blank'><IoLogoLinkedin  className='network' /></a></div>
+              <div> <a href="https://discord.com/channels/@me" target='blank'><FaDiscord  className='network'  /></a></div>
           </div>
     </div>
   )
