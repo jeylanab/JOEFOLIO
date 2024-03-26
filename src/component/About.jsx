b9a88b344c272d78e9254bd9e029e2edd6e370de
jeylanab <jeylantesi@gmail.com>
2024-03-26 20:10:01 +0000

    added placeholder

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -27,9 +27,9 @@ const About = () => {
           <h1 className='whatDo font-semibold lg:text-4xl text-3xl text-center py-10'> Contact Me</h1>
          <form className='flex flex-col justify-center items-center ' ref={form} onSubmit={sendEmail}>
             <label className='whatDo '>Name</label>
-            <input className='lg:w-[400px] lg:h-[40px] w-[200px]' type="text" name="user_name" />
+            <input className='lg:w-[400px] lg:h-[40px] w-[200px] text-start p-3' type="text" name="user_name"  placeholder='jane doe'/>
             <label className='whatDo'>Email</label>
-            <input className='lg:w-[400px] lg:h-[40px] w-[200px]'  type="email" name="user_email" />
+            <input className='lg:w-[400px] lg:h-[40px] w-[200px] text-start p-3'  type="email" name="user_email" placeholder='youremail@gmail.com' />
             <label className='whatDo'>Message</label>
             <textarea className='lg:w-[400px] lg:h-[80px] w-[200px]'  name="message" />
             <input className='lg:w-[400px] bg-purple-600 lg:h-[40px] my-5 rounded-lg w-[200px] p-3 text-slate-200'    type="submit" value="Send" />
