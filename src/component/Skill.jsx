94beb56e2c1327f8c5bdf43f47fb00f59017fb58
jeylanab <jeylantesi@gmail.com>
2024-06-02 17:05:54 +0530

    added skill section

--- src/component/Skill.jsx
+++ src/component/Skill.jsx
@@ -1,77 +1,44 @@
-import React, { useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
-import {  html, javascript, css, adobexd, figma, jquery, react,  tailwind,  vue } from './index'
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
 
+class Skill extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      options: {
+        chart: {
+          id: "basic-bar"
+        },
+        xaxis: {
+          categories: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'TypeScript', 'Next.js', 'GraphQL']
+        }
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [90, 85, 80, 95, 75, 70, 60, 50] // Example percentages, adjust as needed
+        }
+      ]
+    };
+  }
 
-
-const Skill = () => {
-  return (
-      <div className='mt-10 py-10'>
-          <div className='skill flex justify-center items-center'>
-              <div className='skillTitle'>
-                  <h1 className='whatDo font-semibold lg:text-4xl text-3xl'>Skills</h1>
-              </div>
-              
+  render() {
+    return (
+      <div className="Skill">
+        <div className="row">
+          <div className="mixed-chart">
+            <Chart
+              options={this.state.options}
+              series={this.state.series}
+              type="bar"
+              width="500"
+            />
           </div>
-          <div className=' flex items-center justify-center'>
-                    <Swiper className= 'mySkill'
-                    effect={'coverflow'}
-                    grabCursor={true}
-                    centeredSlides={true}
-                    slidesPerView={'auto'}
-                    coverflowEffect={{
-                    rotate: 50,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 1,
-                    slideShadows: true,
-                    }}
-                    pagination={true}
-                    modules={[EffectCoverflow, Pagination]}
-                   
-                >
-                  <SwiperSlide className='mySkillSwiper'>
-                      <img src={html} alt="" />
-                    </SwiperSlide>
-                    <SwiperSlide className='mySkillSwiper'>
-                      <img src={  css } />
-                    </SwiperSlide>
-                    <SwiperSlide className='mySkillSwiper'>
-                      <img src={ javascript} />
-                    </SwiperSlide>
-                    <SwiperSlide className='mySkillSwiper'>
-                    <img src={react} />
-                    </SwiperSlide>
-                    <SwiperSlide className='mySkillSwiper'>
-                    <img src={jquery} />
-                    </SwiperSlide>
-                    <SwiperSlide className='mySkillSwiper'>
-                    <img src={vue} />
-                    </SwiperSlide>
-                    <SwiperSlide className='mySkillSwiper'>
-                      <img src={ tailwind} />
-                    </SwiperSlide>
-                    <SwiperSlide className='mySkillSwiper'>
-                      <img src={figma } />
-                    </SwiperSlide>
-                    <SwiperSlide className='mySkillSwiper'>
-                      <img src={adobexd } />
-                    </SwiperSlide>
-                </Swiper>
-            
-          </div>
-         
-          
-
-
-
-     </div>
-  )
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Skill
+export default Skill;
