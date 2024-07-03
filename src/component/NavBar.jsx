5a3012f639d120ec4a18ac5b3808d469a680a645
jeylanab <jeylantesi@gmail.com>
2024-07-03 15:27:17 +0530

    resposive nav bar added

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -32,11 +32,11 @@ const NavBar = () => {
           <img className='' src={logo} alt="logo" />
           <img onClick={toggleMenu} src={menu} alt="menu" />
           {isMenuOpen && (
-          <div className="absolute top-16 right-0 bg-white shadow-lg mt-2 py-2 w-48 rounded-lg">
+          <div className="absolute top-16 right-0 bg-white shadow-lg opacity-60 mt-2 py-2 w-48 rounded-lg">
               <ul>
                 {navLinks.map((value, index) => (
                   <li className='' >
-                    <a className="block px-4 py-2 text-gray-800 hover:bg-gray-200"  href={value.link}>{value.title}</a></li>
+                    <a className="block px-4 py-2 text-gray-800 hover:bg-gray-200 subtitle"  href={value.link}>{value.title}</a></li>
                   
                    ))}
               </ul>
