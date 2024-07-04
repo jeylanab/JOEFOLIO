602efbd3d4cee0b83e4bbf171c74559ec452fb57
jeylanab <jeylantesi@gmail.com>
2024-07-04 09:24:12 +0530

    added new conponents

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import NavBar from "./component/NavBar";
 import Hero from "./component/Hero";
 import Description from "./component/Description";
 import Footer from "./component/Footer";
+import Projects from "./component/Projects";
+import Contact from "./component/Contact" 
+import Skills from "./component/Skills";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => (
-  <div >
-    <NavBar/>
-    <Hero/>
-    <Description/>
-    <Footer/>
-  </div>
+  <Router >
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<Description />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer/>
+    </div>
+  </Router>
 );
 
 export default App;
