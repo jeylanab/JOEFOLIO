1fb02a64dc1a49822ba90670fcb484e5eff156e2
jeylanab <jeylantesi@gmail.com>
2024-07-05 11:33:23 +0530

    added animation for hero section

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
       <div className=''>
           <div className='text-white text-center flex flex-col items-center justify-center'>
              <h1 className='text-white text-center paragraph  mt-10 anime'>Hello! My Name Is <span className='jey'>Jeylan A</span></h1>
-             <h1 className='text-5xl lg:text-9xl font-bold myshadow anime-h'>WebDeveloper</h1>
-              <h1 className='text-5xl lg:text-9xl font-bold myshadow anime-h'>WebDesigner</h1>
+             <h1 className='text-5xl lg:text-9xl font-bold myshadow anime-r'>WebDeveloper</h1>
+              <h1 className='text-5xl lg:text-9xl font-bold myshadow anime-l'>WebDesigner</h1>
               <img className='w-[400px] mt-10 absolute imgshadow top-20 anime-2' src={jeylan} alt="" />
           </div> 
               
