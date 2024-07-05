a9feefaf9a829d23858c5ab1a15dd26e33bc9a78
jeylanab <jeylantesi@gmail.com>
2024-07-05 11:30:46 +0530

    added keyframe

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -7,9 +7,9 @@ const Hero = () => {
       <div className=''>
           <div className='text-white text-center flex flex-col items-center justify-center'>
              <h1 className='text-white text-center paragraph  mt-10 anime'>Hello! My Name Is <span className='jey'>Jeylan A</span></h1>
-             <h1 className='text-5xl lg:text-9xl font-bold myshadow anime-1'>WebDeveloper</h1>
-              <h1 className='text-5xl lg:text-9xl font-bold myshadow anime-2'>WebDesigner</h1>
-              <img className='w-[400px] mt-10 absolute imgshadow top-20 anime-3' src={jeylan} alt="" />
+             <h1 className='text-5xl lg:text-9xl font-bold myshadow anime-h'>WebDeveloper</h1>
+              <h1 className='text-5xl lg:text-9xl font-bold myshadow anime-h'>WebDesigner</h1>
+              <img className='w-[400px] mt-10 absolute imgshadow top-20 anime-2' src={jeylan} alt="" />
           </div> 
               
       </div>
