7899dbf90b1f90653bcc98a44b7bb96d6c6b8e9c
jeylanab <jeylantesi@gmail.com>
2024-07-12 18:28:31 +0530

    added contact

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='text-white'> 
+      <div className='text-white title myshadow text-center m-10'> Get in touch with Me</div>
+      <div className=' lg:flex md:flex block justify-center items-center m-10'>
+        <div className='card-contact'>
+          <img src="" alt="" />
+          <h1>Email</h1>
+          <p>contact me on joephoenix755@gmail.com</p>
+        </div>
+        <div className='card-contact'>
+            <img src="" alt="" />
+            <h1>Email</h1>
+            <p>contact me on joephoenix755@gmail.com</p>
+        
+        </div>
+        <div className='card-contact'>
+            <img src="" alt="" />
+             <h1>Email</h1>
+             <p>contact me on joephoenix755@gmail.com</p>
+        </div>
+      
+      </div>
+      
+      <div>
+        
+      </div>
+    
+    
+    </div>
   )
 }
 
