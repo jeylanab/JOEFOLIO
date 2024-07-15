b8c32f17e3ed64eb2b055c52e6ada63e7aaba587
jeylanab <jeylantesi@gmail.com>
2024-07-15 10:34:39 +0530

    added form button

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='text-white'> 
+    <div className='text-white m-10'> 
       <div className='text-white title myshadow text-center m-10'> Get in touch with Me</div>
       <div className=' lg:flex md:flex block justify-center items-center m-10'>
         <div className='card-contact'>
@@ -23,6 +23,24 @@ const Contact = () => {
              <h1>Email</h1>
              <p>contact me on joephoenix755@gmail.com</p>
         </div>
+      </div>
+      
+      <div className='flex flex-col justify-center items-center bg '>
+        <div className='my-5 '>
+          <label>Name:</label>
+          <input className='block' type="text" />
+        </div>
+        <div className=' my-5'>
+          <label>Email:</label>
+          <input className='block' type="text" />
+        </div>
+         <div className=' my-5'>
+          <label>Message:</label>
+          <textarea className='block' cols="23" rows="5"></textarea>
+         
+        </div>
+        <button className='btn-3 w-[205px] m-4'>Send Message</button>
+        
       </div>
       
       <div>
