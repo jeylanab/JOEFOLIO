1d92c761cad9abd004d1e3d8beb41723bfa4579c
jeylanab <jeylantesi@gmail.com>
2024-12-09 16:11:47 +0530

    add design to the hero section

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -5,13 +5,20 @@ import joe0 from "../Assets/joe0.svg"
 
 const Hero = () => {
   return (
-      <div className=''>
-          <div className='text-white text-center flex flex-col items-center justify-cente '>
+      <div className='flex justify-around'>
+          <div>
              <h1 className='text-white text-center paragraph opacity-50  mt-10 '>Hello! My Name Is <span className='jey'>Jeylan A</span></h1>
-             <h1 className=' text-5xl lg:text-9xl font-bold myshadow anime-r'>Software Developer</h1>
-              <h1 className='text-5xl lg:text-9xl font-bold myshadow anime-l'>Designer</h1>
-              <img className='mt-10 lg:w-[400px] mt-20 absolute' src={joe0} alt="" />
+          
+          </div>
+          
+          <div className='text-white text-center flex flex-col items-center justify-cente '>
+             
+              <img className='lg:w-[400px]' src={joe0} alt="" />
           </div> 
+          <div>
+              <h1 className=' text-5xl lg:text-9xl font-bold myshadow anime-r'>Software Developer</h1>
+              <h1 className='text-5xl lg:text-9xl font-bold myshadow anime-l'>Designer</h1>
+          </div>
               
       </div>
   )
