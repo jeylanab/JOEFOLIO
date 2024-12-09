78ad3af9a0da00b1ed81a64c497e2584335ac713
jeylanab <jeylantesi@gmail.com>
2024-12-09 16:53:55 +0530

    added svg file

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -5,20 +5,23 @@ import joe0 from "../Assets/joe0.svg"
 
 const Hero = () => {
   return (
-      <div className='flex justify-around'>
+      <div className='flex justify-center'>
+                        
           <div>
-             <h1 className='text-white text-center paragraph opacity-50  mt-10 '>Hello! My Name Is <span className='jey'>Jeylan A</span></h1>
-          
+              <div>
+              </div>
+              <img className='lg:w-[500px] jey' src={joe0} alt="" />
           </div>
-          
           <div className='text-white text-center flex flex-col items-center justify-cente '>
-             
-              <img className='lg:w-[400px]' src={joe0} alt="" />
+              <div>
+                 <h1 className='text-white text-center paragraph opacity-50  mt-10 '>Hi! I am </h1>
+                 <h1 className='jey'>Jeylan A</h1>       
+              </div>
+
+             <h1 className=' '>Software Developer</h1>
+              <h1 className=''>Designer</h1>
           </div> 
-          <div>
-              <h1 className=' text-5xl lg:text-9xl font-bold myshadow anime-r'>Software Developer</h1>
-              <h1 className='text-5xl lg:text-9xl font-bold myshadow anime-l'>Designer</h1>
-          </div>
+
               
       </div>
   )
