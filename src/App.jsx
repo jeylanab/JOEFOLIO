c467979708ae3938b9a13fcf324cf85404062236
jeylanab <jeylantesi@gmail.com>
2025-01-26 12:15:01 +0530

    added new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
-import React from "react";
-import NavBar from "./component/NavBar";
-import Hero from "./component/Hero";
-import Description from "./component/Description";
-import Footer from "./component/Footer";
-import Projects from "./component/Projects";
-import Contact from "./component/Contact" 
-import Skills from "./component/Skills";
-import About from "./component/About";
-import 'font-awesome/css/font-awesome.min.css';
+import React from 'react'
+import { Home } from './Pages/Home'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-const App = () => (
-  <Router >
+const App = () => {
+  return (
     <div>
-      <NavBar />
-      <Routes>
-         <Route
-            path="/"
-            element={
-              <>
-                <Hero />
-                <Description />
-              </>
-            }
-          />
-        <Route path="/" element={<Description />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Footer/>
+      <Home />
     </div>
-  </Router>
-);
+  )
+}
 
-export default App;
+export default App
